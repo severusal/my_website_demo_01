e63c09b8082ae9ee73b99ef2aa63f73c7a2b7240
Severus <severusal1207@gmail.com>
2019-08-07 10:47:40 +0800

    Delete app.py

--- app/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
- 
-app = Flask(__name__)
-@app.route('/')
-def index():
-    return "<h1>Hello, welcome to H2 Computing!</h1>"
-if __name__ == '__main__':
-    app.run(debug=True)
